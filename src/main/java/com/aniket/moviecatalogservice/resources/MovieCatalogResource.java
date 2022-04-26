c2a00b0ebf5901f969abdb54e155dad29231bbbe
Aniket Govekar <aniket.govekar@gmail.com>
2022-04-26 10:03:52 +0530

    1st commit

---
+++ src/main/java/com/aniket/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,46 @@
+package com.aniket.moviecatalogservice.resources;
+
+import com.aniket.moviecatalogservice.models.CatalogItem;
+import com.aniket.moviecatalogservice.models.Movie;
+import com.aniket.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(path = "/catalog")
+public class MovieCatalogResource {
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public MovieCatalogResource(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping(value = "/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1", 5),
+                new Rating("2", 4)
+        );
+
+        return ratings
+                .stream()
+                .map(rating -> {
+
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+                    return new CatalogItem(movie.getName(), "Kolar Gold Fields Part 1", rating.getRating());
+                })
+                .collect(Collectors.toList());
+    }
+}
